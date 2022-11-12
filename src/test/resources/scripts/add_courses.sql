7a40d509a9d35ea41aacc473a8e0f400601d3234
Nurisa <nurisa.abdykalykova03@gmail.com>
2022-11-12 11:21:03 +0600

    commit

--- src/test/resources/scripts/add_courses.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO courses(id,course_description, course_image, course_name)
-VALUES (1,'Hello1','image1','java1');
-INSERT INTO courses(id,course_description, course_image, course_name)
-VALUES (2,'Hello2','image2','java2');
-INSERT INTO courses(id,course_description, course_image, course_name)
-VALUES (3,'Hello3','image3','jav3');
